02db13f5a749fd703eff87277addbdd4db9347ae
Amir R Muntasser <arkmuntasser@gmail.com>
2015-01-14 18:12:58 -0700

    Updated everything
    Updated docs, classed out css better, changed how images are loaded in
    so it doesn't depend on a css class.

--- jquery.ic.js
+++ jquery.ic.js
@@ -1,20 +1,28 @@
 (function( $ ){
-   $.fn.icslide = function(e) {
-	  var parentOffset = $(this).offset(); 
-	  var relX = e.pageX - parentOffset.left;
-	  var parentWidth = $(this).width();
-	  var percentageTravelled = (relX / parentWidth) * 100;
-	  var perecentageRemaining = 100 - percentageTravelled;
-	  
-	  if(percentageTravelled >= 0 && percentageTravelled <= 100) {
-	  	$(this).find(".ic-handle").attr("style", "left: " + percentageTravelled + "%;");
-	  	$(this).find(".ic-left").attr("style", "width: " + percentageTravelled + "%;");
-	  	$(this).find(".ic-right").attr("style", "width: " + perecentageRemaining + "%;");
-	  }
-	  
-	  return this;
-   }; 
+	$.fn.icslide = function(e) {
+		var parentOffset = $(this).offset(),
+			relX = e.pageX - parentOffset.left,
+			parentWidth = $(this).width(),
+			percentageTravelled = (relX / parentWidth) * 100,
+			perecentageRemaining = 100 - percentageTravelled,
+			leftImage = $(this).find(".ic-left").data("bgi"),
+			rightImage = $(this).find(".ic-right").data("bgi");
+
+		if(percentageTravelled >= 0 && percentageTravelled <= 100) {
+		  	$(this).find(".ic-handle").attr("style", "left: " + percentageTravelled + "%;");
+		  	$(this).find(".ic-left").attr("style", "width: " + percentageTravelled + "%; background-image: url('" + leftImage + "')");
+			$(this).find(".ic-right").attr("style", "width: " + perecentageRemaining + "%; background-image: url('" + rightImage + "')");
+		}
+
+		return this;
+	}; 
 })( jQuery );
+
+$(document).ready(function() {
+	$(".ic-image").each(function() {
+		$(this).attr("style", "background-image: url('" + $(this).data("bgi") + "')");
+	});
+});
 
 $(".image-compare").bind('move', function(e) {
 	$(this).icslide(e);
